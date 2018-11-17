dfb18c3590df8629f5ebdab01d7c48ab27dd636f
RICARDO AMADOR CASTANEDA <ricardo.amador@alumnos.udg.mx>
2018-11-17 00:22:36 -0600

    Query sets for tareas works

--- Classroom/models.py
+++ Classroom/models.py
@@ -8,6 +8,8 @@ from django.db import models
 class Usuario(models.Model):
     username = models.OneToOneField(User)
     tipo = models.IntegerField(default=1) # 1 alumno, 2 profesor, 3 admin
+    def __unicode__(self):
+        return self.username.username
 
 
 class Maestro(models.Model):
@@ -16,26 +18,23 @@ class Maestro(models.Model):
     codigo = models.CharField(max_length=20)
     email = models.EmailField()
 
+    def __unicode__(self):
+        return self.usuario.username.username
+
 
 class Materia(models.Model):
     nombre = models.TextField()
-    horas = models.TimeField()  # horas al semestre
-    codigo = models.CharField(max_length=10)
+    horas = models.IntegerField()  # horas al semestre
     departamento = models.TextField()
-
-class MateriaCiclo(models.Model):
-    materia = models.ForeignKey(Materia)
     maestro = models.ForeignKey(Maestro)
     hora = models.TimeField()
     aula = models.CharField(max_length=10)
     ciclo = models.CharField(max_length=15)
     codigo = models.CharField(max_length=10)  # para inscribirse al curso
 
-class Tarea(models.Model):
-    materiaCiclo = models.ForeignKey(MateriaCiclo)
-    descripcion = models.TextField()
-    fechaInicio = models.DateField(auto_now=True)
-    fechaFin = models.DateField()
+    def __unicode__(self):
+        return self.nombre
+
 
 class Alumno(models.Model):
     codigo = models.CharField(max_length=20)
@@ -45,16 +44,30 @@ class Alumno(models.Model):
     telefono = models.TextField()
     email = models.EmailField()
 
+    def __unicode__(self):
+        return self.usuario.username
 
-class TareaAlumno(models.Model):
+class TareaMateria(models.Model): # Tareas para un curso (materia) en especifico
+    materia = models.ForeignKey(Materia)
+    titulo = models.CharField(max_length=40)
+    descripcion = models.TextField()
+    fechaInicio = models.DateField(auto_now=True)
+    fechaFin = models.DateField()
+
+
+class TareaAlumno(models.Model): # Entregas individuales por alumno
     alumno = models.ForeignKey(Alumno)
-    tarea = models.ForeignKey(Tarea)
-    calificacion = models.FloatField(null=True)
-    entrega = models.DateTimeField(null=True)
+    tareaMateria = models.ForeignKey(TareaMateria)
+    calificacion = models.FloatField(null=True, blank=True)
+    entrega = models.DateTimeField(null=True, blank=True)
     comentario = models.TextField(default="Ninguno")
+    estatus = models.IntegerField(default=1)  # 1 pendiente, 2 entregado, 3 atrasado
 
 
 class MateriaAlumno(models.Model):  # MateriaCiclo & alumno (curso)
-    materiaCiclo = models.ForeignKey(MateriaCiclo)
+    materia = models.ForeignKey(Materia)
     alumno = models.ForeignKey(Alumno)
-    calificacion = models.FloatField(null=True)
+    calificacion = models.FloatField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.materia.nombre
--- Classroom/views.py
+++ Classroom/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import *
 
 # Create your views here.
 
@@ -10,4 +11,18 @@ def init(request):
     return render(request, 'index.html')
 
 def studentInit(request):
-    return render(request, 'portalAlumno/index.html')
+
+    student =Alumno.objects.first()
+    materiaAlumno = MateriaAlumno.objects.filter(alumno=1)
+
+    listaTareas = []
+    for curso in materiaAlumno:
+        ta = TareaAlumno.objects.filter(alumno=2)
+        listaTareas.append(ta)
+    print listaTareas
+
+
+    tasks = TareaMateria.objects.filter(materia=1)
+
+    context = {"tareas":tasks}
+    return render(request, 'portalAlumno/index.html', context)
